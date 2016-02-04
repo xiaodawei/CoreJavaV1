fd70332a03a9bc64f5abebfe403239f3306c67d7
Dave Xiao <xdwdave@gmail.com>
2016-02-05 00:23:28 +0800

    Add Welcome.java

---
+++ chapter02/Welcome.java
@@ -0,0 +1,18 @@
+/**
+ * This program displays a greeting from the authors.
+ * @version 1.2@2016-02-04
+ * @author XXX XXXXXXX
+*/
+public class Welcome
+{
+    public static void main(String[] args)
+    {
+        String[] greeting = new String[3];
+        greeting[0] = "Welcome to Core Java";
+        greeting[1] = "by Cay Horstmann";
+        greeting[2] = "and Gary Cornell";
+
+        for (String g : greeting)
+            System.out.println(g);
+    }
+}
