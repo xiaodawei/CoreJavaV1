e042af3a4932b8dd58bb7b56377befa6a0c07588
Dave Xiao <xdwdave@gmail.com>
2016-02-06 23:45:44 +0800

    Add codelist 3-1 ~ 3-4

---
+++ chapter03/FirstSample.java
@@ -0,0 +1,12 @@
+/**
+ * This is the first sample program in Core Java Chapter 3
+ * @version 1.01 2016-02-05
+ * @authors XXX
+ */
+public class FirstSample
+{
+    public static void main(String[] args)
+    {
+        System.out.println("We will not use 'Hello, World!'");
+    }
+}
---
+++ chapter03/InputTest.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+/**
+ * This program demonstrates console input.
+ * @version 1.10 2016-02-06
+ * @author XXX
+*/
+
+public class InputTest
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        // get first input
+        System.out.println("What is your name? ");
+        String name = in.nextLine();
+
+        // get second input
+        System.out.println("How old are you? ");
+        int age = in.nextInt();
+
+        // display output on console
+        System.out.println("Hello, " + name + ". Next year, you'll be " + (age + 1));
+    }
+}
